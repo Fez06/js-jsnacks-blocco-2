1d5051336289954f82009c560300bde58ac7a717
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-27 11:59:43 +0100

    fatto

--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 //JSsnack2
 //Inserisci un numero, se è pari stampa il numero,
 //se è dispari stampa il numero successivo
+
+const valore = number(prompt('inserisci un numero'));
+
+if (valore % 2 === 0) {
+    console.log(valore);
+} else {
+    console.log(valore + 1);
+};
