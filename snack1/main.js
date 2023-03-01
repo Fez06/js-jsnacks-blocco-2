4e5928d3214051c9080a6fe8199247d85ab967f1
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-01 11:05:19 +0100

    while

--- snack1/main.js
+++ snack1/main.js
@@ -6,14 +6,23 @@
 //Il programma stampa la somma di tutti i numeri inseriti.
 //Esegui questo programma in due versioni, con il for e con il while.
 
+                 //for//
+// let somma = 0;
 
+// for (let i = 0; i < 5; i++) {
+    
+//     let addendo = Number(prompt('inserisci un numero:'));
+//     somma += addendo;
+//     console.log(somma);
+// }
+
+                  //while//
+let i = 0;
 let somma = 0;
 
-for (let i = 0; i < 5; i++) {
-    
+while ( i < 5 ) {
     let addendo = Number(prompt('inserisci un numero:'));
     somma += addendo;
     console.log(somma);
+    i++;
 }
-
-
